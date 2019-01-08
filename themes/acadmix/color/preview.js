db20e29f0424bc2e35781144530bf2b97210bc2a
nitesh624 <nsinha624@gmail.com>
2019-01-08 12:41:32 +0530

    make top nav branding to be configured from theme settings

--- themes/acadmix/color/preview.js
+++ themes/acadmix/color/preview.js
@@ -15,7 +15,7 @@
       var $colorPalette = $form.find('.js-color-palette');
 
 
-      //$colorPreview.find('.color-preview-header').css('background-color', $colorPalette.find('input[name="palette[topnav]"]').val());
+      $colorPreview.find('.color-preview-header').css('color', $colorPalette.find('input[name="palette[topnav]"]').val());
       // $colorPreview.find('').css('color', $colorPalette.find('input[name="palette[topnavtext]"]').val());
       $colorPreview.find('.color-preview-header, .color-preview-footer-wrapper').css('background-color', $colorPalette.find('input[name="palette[footer]"]').val());
 
