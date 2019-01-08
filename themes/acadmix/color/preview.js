d50fba729b2f726fd4d7ef7a02b98593533207f6
nitesh624 <nsinha624@gmail.com>
2019-01-08 20:16:59 +0530

    changes in theme

--- themes/acadmix/color/preview.js
+++ themes/acadmix/color/preview.js
@@ -16,9 +16,8 @@
 
 
       $colorPreview.find('.color-preview-header *, .color-preview-footer-wrapper *').css('color', $colorPalette.find('input[name="palette[topnav]"]').val());
-      // $colorPreview.find('').css('color', $colorPalette.find('input[name="palette[topnavtext]"]').val());
       $colorPreview.find('.color-preview-header, .color-preview-footer-wrapper').css('background-color', $colorPalette.find('input[name="palette[footer]"]').val());
-      // $colorPreview.find('.color-preview-main h2, .color-preview .preview-content').css('color', $colorPalette.find('input[name="palette[text]"]').val());
+      $colorPreview.find('.color-preview-main h2, .color-preview .preview-content').css('color', $colorPalette.find('input[name="palette[text]"]').val());
       $colorPreview.find('.color-preview-content a').css('color', $colorPalette.find('input[name="palette[link]"]').val());
     }
   };
